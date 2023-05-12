5a77396731ebaf621f35b425c423c6b8af8e3663
Javier <javicairoli3@gmail.com>
2023-05-12 10:48:57 -0300

    contacto

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ const dataJson = async () => {
     console.log(data.servicio);
     const boxClientes= document.getElementById('boxClientes');
     const boxServicios= document.getElementById('boxServicios');
-    crearCards(cliente, boxClientes);
-    crearCards(servicio, boxServicios);
-  
+    crearCardsClientes(cliente, boxClientes);
+    crearCardsServicios(servicio, boxServicios); 
+     
   }
 
   catch (error) {
@@ -21,7 +21,7 @@ const dataJson = async () => {
 
 dataJson()
 
-const crearCards=(array, conteiner) =>{ 
+const crearCardsServicios=(array, conteiner) =>{ 
     let cards=""
     if(array.length == 0){
         conteiner.innerHTML = "<h2 class='display-1 fw-bolder'>No hay coincidencias!</h2>"
@@ -31,6 +31,26 @@ const crearCards=(array, conteiner) =>{
       array.forEach((evento) => {
         cards += `<div class="card" style="width: 16rem;">
         <a href="./descripcion.html?id=${evento._id}" class="btn btn-primary"><img src=${evento.image} class="card-img-top" alt="..." style="height: 11rem;width: 14rem;"></img></a>
+        <div class="card-body">
+          <h5 class="card-title">${evento.name}</h5>
+          </div>
+          </div>`
+      })
+  
+        
+    conteiner.innerHTML = cards
+  }
+
+  const crearCardsClientes=(array, conteiner) =>{ 
+    let cards=""
+    if(array.length == 0){
+        conteiner.innerHTML = "<h2 class='display-1 fw-bolder'>No hay coincidencias!</h2>"
+        return
+    }
+  
+      array.forEach((evento) => {
+        cards += `<div class="card" style="width: 16rem;">
+        <img src=${evento.image} class="card-img-top" alt="..." style="height: 11rem;width: 14rem;"></img>
         <div class="card-body">
           <h5 class="card-title">${evento.name}</h5>
           </div>
